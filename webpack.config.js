b9bc139becdd7ee399311806b3334ebf5da17884
NataliaMarkaki <markaki.natalia@gmail.com>
2022-06-03 13:17:15 +0100

    feat(app): add initial set up

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    hot: true,
+    port: 3000,
+    compress: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, './index.html'), })
+  ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,11 @@
+const config = require('./webpack.config');
+const path = require('path');
+
+module.exports = {
+  ...config,
+  mode: 'production',
+  devtool: 'source-map',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+  },
+};
